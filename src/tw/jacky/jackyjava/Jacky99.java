a61fe78f7b53ba987d131ebb6059ef71f751b2ab
JackyLin0 <lhm0531@gmail.com>
2016-08-20 13:57:14 +0800

    99  乘法表

--- src/tw/jacky/jackyjava/Jacky99.java
+++ src/tw/jacky/jackyjava/Jacky99.java
@@ -2,29 +2,21 @@ package tw.jacky.jackyjava;
 
 public class Jacky99 {
 
-	public static void main(String[ ] args)
-	{
-		
-		
-		for(int i=1;i<=9;i++)
-		{
-			for(int j=2;j<=5;j++)
-			{
-				System.out.print(j+"*"+i+"＝"+i*j+"\t"); 
-				
+	public static void main(String[] args) {
+
+		for (int k = 0; k <= 1; k++) {
+			for (int i = 1; i <= 9; i++) {
+
+				for (int j = 2; j <= 5; j++) {
+					System.out.print(( j+k*4) + "*" + i + "＝" + i * ( j+k*4) + "\t");
+
+				}
 			}
 			System.out.println("\n");
-			
-			
-			
+
+			System.out.println("------------------");
 		}
-		
-		
-		
-		
+
 	}
-
-
-
 
 }
