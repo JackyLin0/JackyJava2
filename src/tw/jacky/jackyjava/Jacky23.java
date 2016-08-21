f265d13d4e8427d64e5d4c76e3918ac5edf0fdc5
JackyLin0 <lhm0531@gmail.com>
2016-08-21 13:05:22 +0800

    while 語法

---
+++ src/tw/jacky/jackyjava/Jacky23.java
@@ -0,0 +1,17 @@
+package tw.jacky.jackyjava;
+
+// while 語法
+public class Jacky23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<10)
+		{
+			System.out.println(i++);
+		}
+		
+		
+	}
+
+}
