8b96761010a4e0e47bc3215a5b2a5c66c4a88228
JackyLin0 <lhm0531@gmail.com>
2016-08-21 12:37:38 +0800

    Brad 版的作弊的骰子

--- src/tw/jacky/jackyjava/Jacky19.java
+++ src/tw/jacky/jackyjava/Jacky19.java
@@ -2,27 +2,23 @@ package tw.jacky.jackyjava;
 
 public class Jacky19 {
 
+	
+	// 作弊的骰子
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] p=new int[10];
+		int [] p=new int[6];
 		
-		for(int i=0;i<=100;i++)
+		for(int i=0;i<=10000;i++)
 		{
-			int point= (int)(Math.random()*10);
-			p[point]++; 
+			int point= (int)(Math.random()*9);
+			p[point>=6?point-3:point]++; 
 	 	}
-			p[3]=p[3]+p[6];
-			p[4]=p[4]+p[7];
-			p[5]=p[5]+p[8];
+			 
 		
 		for(int i=0;i<p.length;i++)
 		{
-			if(i<6){
-			System.out.println((i+1)+":"+p[i]);}
+ 			System.out.println((i+1)+":"+p[i]);}
 		}
 		
-		
-		
-	}
-
+ 
 }
